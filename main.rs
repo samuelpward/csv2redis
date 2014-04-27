32f93a9e3d0e973869d36cd9158fd3e3f219d904
Samuel Ward <samuelpward@gmail.com>
2014-04-27 18:44:37 +1000

    init
    Signed-off-by: Samuel Ward <samuelpward@gmail.com>

---
+++ main.rs
@@ -0,0 +1,30 @@
+extern crate csv;
+
+use std::path::Path;
+use std::os;
+use std::io::Process;
+use std::io::pipe::PipeStream;
+
+
+fn main() {
+  let args = os::args();
+
+  //  Opens and decodes csv file (with header)
+  let fp = &Path::new(args[1]);
+  let mut rdr = csv::Decoder::from_file(fp);
+  rdr.has_headers(true);
+  
+  //  Start redis-cli --pipe
+  let mut child = match Process::new("redis-cli", ["--pipe".to_owned()]) {
+    Ok(child) => child,
+    Err(e) => fail!("Failed. Do you have redis installed?"),
+  };
+
+  //  stdin pipe for redis-cli
+  let mut pipe = PipeStream::open(child.stdin.get_ref());
+    
+  for (date, open, high, low, close, volume, adj) in rdr.decode_iter::<(~str, f32, f32, f32, f32, uint, f32)>() {
+      pipe.write_line("SET {} {}", date, close);
+    }
+
+}
